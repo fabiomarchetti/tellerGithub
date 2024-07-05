91a8b21f5aabdb14020e1ab9ebde48a3c5a2525b
fabio <marchettisoft@gmail.com>
2024-07-05 10:45:48 +0200

    Update main.py
    main

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import cv2
-import numpy as np
+import sqlite3
+from sqlite3 import Error
+import time
+import datetime
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.uic import loadUi
+from PyQt5 import uic
+
+from PyQt5.QtWidgets import QMainWindow
